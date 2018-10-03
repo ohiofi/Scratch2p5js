af69221af7696c5ca7364cef2ac7ea9d7e6a5b04
Justin Riley <20hz20khz@gmail.com>
2018-10-03 10:20:48 -0400

    cleaned up some method comments

--- scratch2p5js/sprite.js
+++ scratch2p5js/sprite.js
@@ -1,6 +1,9 @@
 /*global ADD,CENTER,DEGREES,DOWN_ARROW,LEFT,LEFT_ARROW,RADIANS,RIGHT,RIGHT_ARROW,UP_ARROW,angleMode,background,beginShape,blendMode,color,cos,createCanvas,curveVertex,displayHeight,displayWidth,dist,ellipse,endShape,fill,floor,frameCount,frameRate,height,image,img,keyCode,keyIsDown,keyIsPressed,line,loadImage,loadSound,mouseIsPressed,mouseX,mouseY,noFill,noStroke,pop,push,radians,random,rect,rotate,round,scale,shuffle,sin,stroke,strokeWeight,text,textAlign,textSize,tint,translate,triangle,width,windowHeight,windowWidth */
 
 class Sprite {
+	/*
+	    constructor with optional arguments. need to add a base64 sprite if user doesn't specify an animation array.
+	*/
   constructor(animationArray, x, y, radius, width, height) {
     if(arguments.length == 0) {
       // probably should just load a default base64 sprite (like Scratch cat) if the user forgot to specify an image
@@ -68,11 +71,12 @@ class Sprite {
         this.imageScale = this.width / this.animationArray[0].width;
     }
   }
+  /*
+      animate(_startPosition,_endPosition)
+      Loop through the images in the animation array
+      Arguments optional
+  */
   animate(_startPosition,_endPosition) {
-    /*
-    Loop through the images in the animation array
-    Arguments optional
-    */
     switch (arguments.length) {
       case 2:
         // loop through images from animationArray[startPosition] through animationArray[endPosition] inclusive
@@ -92,11 +96,13 @@ class Sprite {
         this.switchCostumeTo(floor(this.animationCounter) % this.animationArray.length);
     }
   }
+  /*
+      blink(someFlashSpeed)
+      Will flash the sprite at a given rate, once every someFlashSpeed frames.
+      For example, blink(10) will flash the sprite every 10th frame.
+      Arguments optional, will default to once every 5 frames if no arguments.
+  */
   blink(someFlashSpeed) {
-    /*
-    Will flash the sprite at a given rate, once every someFlashSpeed frames.
-    Arguments optional, will default to once every 5 frames if no arguments.
-    */
     switch (arguments.length) {
       case 0:
         someFlashSpeed = 5
@@ -108,82 +114,146 @@ class Sprite {
       this.flashEnabled = false;
     }
   }
+  /*
+  	changeHeightBy(somePercent)
+  	change the collider height by some percent
+  */
   changeHeightBy(somePercent) {
-    // change the collider height by some percent
     this.height *= (1 + somePercent / 100);
   }
+  /*
+  	changeRadiusBy(somePercent)
+  	change the collider radius by some percent
+  */
   changeRadiusBy(somePercent) {
-    // change the collider radius by some percent
+
     this.radius *= (1 + somePercent / 100);
   }
+  /*
+  	changeSizeBy(somePercent)
+  	change the image scale, collider height, collider radius, and collider width by some percent
+    changeSizeBy(10) will change the size by 10 percent
+  */
   changeSizeBy(somePercent) {
-    // change the image scale, collider height, collider radius, and collider width by some percent
-    // changeSizeBy(10) will change the size by 10 percent
     this.imageScale *= (1 + somePercent / 100);
     this.changeHeightBy(somePercent);
     this.changeRadiusBy(somePercent);
     this.changeWidthBy(somePercent);
   }
+  /*
+  	changeWidthBy(somePercent)
+  	change the collider width by some percent
+  */
   changeWidthBy(somePercent) {
-    // change the collider width by some percent
     this.width *= (1 + somePercent / 100);
   }
+  /*
+  	flash()
+  	set the blendMode to ADD, making the sprite bright white for 1 frame
+  */
   flash() {
-    // set the blendMode to ADD, making the sprite bright white for 1 frame
     this.flashEnabled = true;
     this.show()
     this.flashEnabled = false;
   }
+  /*
+
+  */
   getHeight() {
     return (this.height)
   }
+  /*
+
+  */
   getImageHeight() {
     // returns the scaled image height
     return (this.animationArray[0].height * this.imageScale)
   }
+  /*
+
+  */
   getImageWidth() {
     // returns the scaled image width
     return (this.animationArray[0].width * this.imageScale)
   }
+  /*
+
+  */
   getRadius() {
     return (this.radius)
   }
+  /*
+
+  */
   getWidth() {
     return (this.width)
   }
+  /*
+
+  */
   getX() {
     return (this.x)
   }
+  /*
+
+  */
   getY() {
     return (this.y)
   }
+  /*
+
+  */
   hideSprite() {
     //?
   }
+  /*
+
+  */
   nextCostume() {
     this.animationCounter++;
     this.switchCostumeTo(this.animationArray[floor(this.animationCounter) % this.animationArray.length]);
   }
+  /*
+
+  */
   rotate(someAmount) {
     //?
   }
+  /*
+
+  */
   setAnimationSpeed(newSpeed) {
     //?
   }
+  /*
+
+  */
   setDebug(trueOrFalse) {
     this.debugMode= trueOrFalse;
   }
+  /*
+
+  */
   setDebugOff() {
     this.debugMode= false;
   }
+  /*
+
+  */
   setDebugOn() {
     this.debugMode= true;
   }
+  /*
+
+  */
   setDirection(oneOrNegativeOne) {
     if(oneOrNegativeOne === 1 || oneOrNegativeOne === -1){ // only allow 1 or -1
       this.direction = oneOrNegativeOne;
     }
   }
+  /*
+
+  */
   setHeight(someNumber) {
     /*
     set the collider width
@@ -191,6 +261,9 @@ class Sprite {
     */
     this.height = someNumber
   }
+  /*
+
+  */
   setImageHeight(someNumber) {
     /*
     set the image height
@@ -199,6 +272,9 @@ class Sprite {
     */
     this.imageScale = someNumber / this.animationArray[0].height;
   }
+  /*
+
+  */
   setImageWidth(someNumber) {
     /*
     set the image width
@@ -207,6 +283,9 @@ class Sprite {
     */
     this.imageScale = someNumber / this.animationArray[0].height;
   }
+  /*
+
+  */
   setSizeTo(somePercent) {
     /*
     set the sprite's size to specified % of original image size
@@ -218,6 +297,9 @@ class Sprite {
     this.setRadius(this.radius * this.imageScale);
     this.setWidth(this.width * this.imageScale);
   }
+  /*
+
+  */
   setRadius(someNumber) {
     /*
     set the collider radius
@@ -229,6 +311,9 @@ class Sprite {
     this.setWidth(this.radius * 2);
 
   }
+  /*
+
+  */
   setWidth(someNumber) {
     /*
     set the collider width
@@ -238,14 +323,23 @@ class Sprite {
     this.width = someNumber
     this.radius = this.width / 2;
   }
+  /*
+
+  */
   setX(someNumber) {
     //sets x to some number
       this.x = someNumber;
   }
+  /*
+
+  */
   setY(someNumber) {
     //sets y to some number
       this.y = someNumber;
   }
+  /*
+
+  */
   show() {
     if(this.showSprite) {
       push();
@@ -272,9 +366,15 @@ class Sprite {
       }
     }
   }
+  /*
+
+  */
   showSprite() {
     //?
   }
+  /*
+
+  */
   switchCostumeTo(someNumber) {
     //?
   }
